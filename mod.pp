d7730b9b15c9f512b7ba52dced34756e96820c3e
misraved <ved@turbot.com>
2024-03-06 21:39:42 +0530

    Update social graphics and icon for the mod

--- mod.pp
+++ mod.pp
@@ -3,12 +3,12 @@ mod "nba" {
   description   = "Dive into the dynamic world of NBA with the NBA Dashboard, offering a holistic view of player and team statistics, game performances, and global influence, featuring in-depth analysis through visual comparisons of home vs. away wins, player experience distribution, and international player representation, all meticulously compiled using SQLite with Powerpipe."
   color         = "#C8102E"
   documentation = file("./docs/index.md")
-  icon          = "/images/mods/turbot/nba-dashboard.svg"
+  icon          = "/images/mods/turbot/nba-insights.svg"
   categories    = ["dashboard", "sqlite"]
 
   opengraph {
     title       = "Powerpipe Mod for NBA"
     description = "Dive into the dynamic world of NBA with the NBA Dashboard, offering a holistic view of player and team statistics, game performances, and global influence, featuring in-depth analysis through visual comparisons of home vs. away wins, player experience distribution, and international player representation, all meticulously compiled using SQLite with Powerpipe."
-    image       = "/images/mods/turbot/nba-dashboard-social-graphic.png"
+    image       = "/images/mods/turbot/nba-insights-social-graphic.png"
   }
 }
